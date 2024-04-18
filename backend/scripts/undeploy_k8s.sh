593f277cd0f12474757d39bcacf20979afca8a1c
salomondush <63796975+salomondush@users.noreply.github.com>
2024-04-18 02:23:27 -0500

    Undeploy Kubernetes resources and fix duplicate deletion in undeploy_k8s.sh

--- backend/scripts/undeploy_k8s.sh
+++ backend/scripts/undeploy_k8s.sh
@@ -1,7 +1,7 @@
-kubectl delete -f ../k8s/components.yaml
-
+kubectl delete -f ../k8s/kube2iam.yaml
 kubectl delete -f ../k8s/fastapi-deployment.yaml
 kubectl delete -f ../k8s/fastapi-service.yaml
-
 kubectl delete hpa fastapi-deployment
+
+kubectl delete -f ../k8s/components.yaml
 
